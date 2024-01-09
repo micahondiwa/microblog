bc83b732e62b050b12e0c113ef59ff7febfc4f99
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-09 11:37:06 +0300

    Templates

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,17 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "Micah"}
+    return (
+        """
+<html>
+    <head>
+    <title> Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, """
+        + user["username"]
+        + """!</h1>
+    </body>
+</html>"""
+    )
