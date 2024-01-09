8fed2fd2fd691902cb3a27177ff673b3fc3cab35
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-09 11:40:55 +0300

    Render template

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 
@@ -5,16 +6,4 @@ from app import app
 @app.route("/index")
 def index():
     user = {"username": "Micah"}
-    return (
-        """
-<html>
-    <head>
-    <title> Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, """
-        + user["username"]
-        + """!</h1>
-    </body>
-</html>"""
-    )
+    return render_template("index.html", title="Home", user=user)
