7eb0d5a9c0d9d5cd22e07cf17db452b84b9767fc
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-22 11:26:04 +0300

    Model for orm

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from typing import Optional
+import sqlachemy as sa 
+import sqlachemy.orm as so 
+from app import db
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __rep__(self):
+        return '<User {}>'.format(self.username)
