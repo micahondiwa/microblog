dca3fc7df3a1c12a28a7187afe8a13d8787fb062
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-22 08:56:58 +0300

    Forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,12 +8,21 @@ def index():
     user = {'username': 'Micah'}
     posts = [
         {
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in Portland'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
             'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was cool!'
+            'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home',user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
