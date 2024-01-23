121818456aedd77366a964e4ff1ae46c51ac5796
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-23 10:35:50 +0300

    Implement a user log in

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy  as sa
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -19,10 +22,16 @@ def index():
     return render_template('index.html', title='Home',user=user, posts=posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenicated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalars(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
