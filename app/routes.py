2ee5f6e31b8a6425246b0b27f6a59ebce4e5d765
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-25 07:45:29 +0300

    Validate form

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,19 @@
+from datetime import datetime, timezone
 from urllib.parse import urlsplit
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 import sqlalchemy as sa
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
-from app.forms import EditProfileForm
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
@@ -63,6 +71,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -72,6 +81,7 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -85,6 +95,6 @@ def edit_profile():
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
-        form.about_me.data = curren_user.about_me
+        form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
-    form=form)
+                           form=form)
