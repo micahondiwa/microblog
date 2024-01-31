d3b57281e20e5f1aa840a205cdfddc336e246a66
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-31 19:01:49 +0300

    Fix

--- app/routes.py
+++ app/routes.py
@@ -92,7 +92,7 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash('Your changes have been saved.')
+        flash('Your changes have been saved')
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
